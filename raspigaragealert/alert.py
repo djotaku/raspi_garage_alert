cd064772ad7b8ddc56fa14d7650b8126d9ce46f2
Eric Mesa <ericsbinaryworld@gmail.com>
2021-05-29 20:53:05 +0100

    Check if can reach Matrix server
    If the internet is out and the status changed, it would get stuck
    waiting for Matrix to timeout and wouldn't update MQTT. Hopefully
    this fixes it.

--- raspigaragealert/alert.py
+++ raspigaragealert/alert.py
@@ -1,14 +1,23 @@
+import socket
 import xdgenvpy
 
 from raspigaragealert import garage_door as door
 from raspigaragealert.services import mqtt
 from raspigaragealert.services import matrix
 
+def check_matrix(server):
+    host = socket.gethostbyname(server)
+    try:
+        connection = socket.create_connection((host, 80), 2)
+        return True
+    except:
+        pass
+    return False
 
 def main():
     xdg = xdgenvpy.XDGPedanticPackage('raspigaragealert')
     my_door = door.door(7)
-    mqtt_service = mqtt.Publisher(xdg)  # testing values - will need to make user configurable
+    mqtt_service = mqtt.Publisher(xdg) 
     my_matrix_bot = matrix.MatrixBot(xdg)
     loop = True
     success = False
@@ -17,7 +26,8 @@ def main():
         if door_state_changed:
             print(f"Garage door is now {state_in_words}.")
             success = mqtt_service.publish(state_in_words)
-            my_matrix_bot.main(f"Garage door is now {state_in_words}.")
+            if check_matrix(my_matrix_box.config.get('server')):
+                my_matrix_bot.main(f"Garage door is now {state_in_words}.")
         if not success:
             success = mqtt_service.publish(state_in_words)
         # loop = False
